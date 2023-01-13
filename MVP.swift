6c1b032447fa4d21c10c93e9b3e4a3d81b1d21c0
Yılmaz Edis <ylmazedis@gmail.com>
2023-01-13 16:00:58 +0300

    Create MVP.swift

---
+++ MVP.swift
@@ -0,0 +1,38 @@
+// The Model
+class DataModel {
+    var data: String = ""
+}
+
+// The View
+protocol ViewDelegate: class {
+    func updateView(data: String)
+}
+
+class ViewController: UIViewController, ViewDelegate {
+    @IBOutlet weak var dataLabel: UILabel!
+    var presenter: Presenter!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = Presenter(view: self)
+    }
+
+    func updateView(data: String) {
+        dataLabel.text = data
+    }
+}
+
+// The Presenter
+class Presenter {
+    weak var view: ViewDelegate?
+    var model = DataModel()
+
+    init(view: ViewDelegate) {
+        self.view = view
+    }
+
+    func loadData() {
+        model.data = "Some data"
+        view?.updateView(data: model.data)
+    }
+}
